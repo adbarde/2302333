49b6533a4a315dc7a25a8aca94c05824d2b9c53a
adbarde <146936809+adbarde@users.noreply.github.com>
2024-10-08 21:25:52 +0530

    week 12

---
+++ w12_3.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+class First{
+    public:
+    int x;
+    virtual void print(int x1)
+    {
+        x=x1;
+        cout<<"x = "<<x<<endl;
+    }
+};
+class Second: public First{
+    public:
+    int y;
+    void print(int y1)
+    {
+        y=y1;
+        cout<<"y = "<<y<<endl;
+    }
+};
+int main(void)
+{
+    First *ptr;
+    Second obj;
+    ptr = &obj;  
+    ptr->print(10);  
+}
